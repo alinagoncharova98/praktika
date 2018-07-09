a79358f9a56f21f588a5cb9c96b52c80b8aaf0d3
alinagoncharova98 <alina.goncharova98@gmail.com>
2018-07-09 15:35:00 +0300

    Add 4_3

---
+++ 4_3.py
@@ -0,0 +1,10 @@
+from math import *
+from sys import *
+
+n = 1
+while (n < len(argv)):
+    if float(argv[n]) > 0:
+        print("ln(%g)" % float(argv[n]), " = ", log(float(argv[n])))
+    else:
+        print("ln(%g)" % float(argv[n]), " is illegal")
+    n += 1
